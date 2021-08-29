9557815c85f8c0d9dd81ddab3bdbfec1029b9bf7
AhmedGhanam <yghnam334455@gmail.com>
2021-08-29 03:12:09 +0200

    add firebase1

--- lib/firebase/secound_screen2.dart
+++ lib/firebase/secound_screen2.dart
@@ -6,31 +6,64 @@ import 'package:riverpod_statemanagement/firebase/user_model.dart';
 CollectionReference userDate = FirebaseFirestore.instance.collection('users');
 final getUserDate = FutureProvider<QuerySnapshot>((ref) => userDate.get());
 
+final getUserDate_streem = StreamProvider<QuerySnapshot>((ref) =>
+  userDate.snapshots(),
+);
+
 class SecondScreen2 extends ConsumerWidget {
   @override
   Widget build(BuildContext context, watch) {
-    final data = watch(getUserDate);
+
 
     return Scaffold(
       body: Container(
-        child: Center(
-          child: data.when(data: (QuerySnapshot value) {
-            UserModel userModel = UserModel.fromMap(value.docs[0].data());
-            return Center(
-              child: Column(
-                children: [
-                  Text(userModel.name),
-                  Text(userModel.email),
-                ],
-              ),
-            );
-          }, loading: () {
-            return CircularProgressIndicator();
-          }, error: (value, stack) {
-            return Text('error');
-          }),
-        ),
+        child:  getUserDataStream(watch),
+        // _getdata(watch),
       ),
     );
   }
+
+ Widget _getdata(watch){
+   final data = watch(getUserDate);
+   return Center(
+     child: data.when(data: (QuerySnapshot value) {
+       UserModel userModel = UserModel.fromMap(value.docs[0].data());
+       return Center(
+         child: Column(
+           children: [
+             Text(userModel.name),
+             Text(userModel.email),
+           ],
+         ),
+       );
+     }, loading: () {
+       return CircularProgressIndicator();
+     }, error: (value, stack) {
+       return Text('error');
+     }),
+   );
+  }
+
+
+}
+
+Widget getUserDataStream( watch) {
+  final stream =watch(getUserDate_streem);
+  return stream.when(
+      data: (QuerySnapshot value) {
+        UserModel userModel = UserModel.fromMap(value.docs[0].data());
+        return Center(
+          child: Column(
+            children: [
+              Text(userModel.name),
+              Text(userModel.email),
+            ],
+          ),
+        );
+      },
+       loading: () {
+    return CircularProgressIndicator();
+  }, error: (value, stack) {
+    return Text('error');
+  });
 }
